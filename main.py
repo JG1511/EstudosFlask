38617d15cab886e2a599656bdd7dcf1bbeb69c06
JG1511 <joaotsi2024@gmail.com>
2025-04-11 16:03:08 -0300

    implementação do url_for

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from flask import Flask 
+from flask import Flask, url_for
+# o url_for serve para montar urls internas do nosso servidor 
+
 # inicialização
 app = Flask(__name__) ## Apenas para o flask se organizar
 
@@ -6,7 +8,8 @@ app = Flask(__name__) ## Apenas para o flask se organizar
 @app.route("/") # criação da rota principal, por padrão ela vem com método 
 
 def ola_mundo(): # função que aparece ao entrar na página
-    return 'tudo bão?'
+    return f"<a href = '{ url_for('sobre_o_criador')}'>Página sobre"
+
 @app.route("/sobre")
 
 def sobre_o_criador():
