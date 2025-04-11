de653f436a96cb84fe61eb257fa773465915a8f0
JG1511 <joaotsi2024@gmail.com>
2025-04-11 15:57:38 -0300

    implementação de uma rota teste

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ app = Flask(__name__) ## Apenas para o flask se organizar
 
 def ola_mundo(): # função que aparece ao entrar na página
     return 'tudo bão?'
+@app.route("/sobre")
+
+def sobre_o_criador():
+    return 'Meu nome é João Guilherme e eu tenho 19 anos'
 
 # execução
 app.run(debug = True) ## Para o Flask entender que estamos no modo de desenvolvedor; a porta padrão irá ser a 5000
