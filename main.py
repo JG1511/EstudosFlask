57dd5184596a1de015de3d7e7eaaef3d4f1df989
JG1511 <joaotsi2024@gmail.com>
2025-04-11 16:14:00 -0300

    implementação do html e a junção com o python

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, render_template
 # o url_for serve para montar urls internas do nosso servidor 
+# render_template serve para buscar um arquivo para o html ou até mesmo para comunicação do back e o front
 
 # inicialização
 app = Flask(__name__) ## Apenas para o flask se organizar
@@ -8,7 +9,7 @@ app = Flask(__name__) ## Apenas para o flask se organizar
 @app.route("/") # criação da rota principal, por padrão ela vem com método 
 
 def ola_mundo(): # função que aparece ao entrar na página
-    return f"<a href = '{ url_for('sobre_o_criador')}'>Página sobre"
+    return render_template('index.html')
 
 @app.route("/sobre")
 
