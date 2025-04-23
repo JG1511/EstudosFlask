993fae60b12c46b40b82e184a4b53193465c0e11
JG1511 <joaotsi2024@gmail.com>
2025-04-23 17:16:17 -0300

    implementação da pasta de Rotas

--- main.py
+++ main.py
@@ -1,26 +1,13 @@
-from flask import Flask, url_for, render_template
+from flask import Flask
+from routes.home import home_route
 # o url_for serve para montar urls internas do nosso servidor 
 # render_template serve para buscar um arquivo para o html ou até mesmo para comunicação do back e o front
 
 # inicialização
 app = Flask(__name__) ## Apenas para o flask se organizar
 
+app.register_blueprint(home_route)
 # rotas
-@app.route("/") # criação da rota principal, por padrão ela vem com método 
-
-def ola_mundo(): # função que aparece ao entrar na página
-    titulo = "Gestão de Usuários"
-    usuarios = [
-        {"nome":"João", "membro_ativo": True},
-        {"nome":"Lucas", "membro_ativo": False},
-        {"nome":"Jorge", "membro_ativo": False}
-    ]
-    return render_template('index.html', titulo = titulo, usuarios = usuarios)
-
-@app.route("/sobre")
-
-def sobre_o_criador():
-    return 'Meu nome é João Guilherme e eu tenho 19 anos'
 
 # execução
 app.run(debug = True) ## Para o Flask entender que estamos no modo de desenvolvedor; a porta padrão irá ser a 5000
---
+++ routes/home.py
@@ -0,0 +1,7 @@
+from flask import Blueprint,render_template # o Blueprint serve para o gerenciamento das rotas
+
+home_route = Blueprint('home',__name__) # Criação de uma variável para armazenzar o blueprint/rota
+
+@home_route('/')
+def home():
+    return render_template('index.html')
